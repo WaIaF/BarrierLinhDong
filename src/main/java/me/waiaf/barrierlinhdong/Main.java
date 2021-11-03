99f466de253d8f551b97f04b86d9a245b1c254a0
WaIaF <nguyenthanhky441@gmail.com>
2021-11-03 17:03:44 +0700

    meme

--- src/main/java/me/waiaf/barrierlinhdong/Main.java
+++ src/main/java/me/waiaf/barrierlinhdong/Main.java
@@ -13,103 +13,61 @@ import java.util.HashMap;
 
 public final class Main extends JavaPlugin implements Listener {
 
-    public static HashMap<Player, Block> playerLastBlock = new HashMap<>();
+    public HashMap<Player, Block> playerLastBlock = new HashMap<>();
 
     @Override
     public void onEnable() {
-
         this.getServer().getPluginManager().registerEvents(this, this);
-
     }
 
     @EventHandler
-    public void onNiggaMove(PlayerMoveEvent event){
-
-        if (!event.getPlayer().isFlying()){
-
-            Player player = event.getPlayer();
-            Block block = player.getLocation().getBlock();
-
-            if (player.isSneaking() && !block.getType().equals(Material.WATER) && block.getType().equals(Material.AIR)){
-
-                if (!playerLastBlock.containsKey(player)){
-
-                    for (Player otherPlayers : Bukkit.getOnlinePlayers()){
-
-                        if (otherPlayers != null){
-
-                            if (!otherPlayers.getDisplayName().equals(player.getDisplayName())){
-
-                                otherPlayers.sendBlockChange(player.getLocation(), Material.BARRIER.createBlockData());
-                                playerLastBlock.put(player, block);
-
-                            }
-
-                        }
-
+    public void onNiggaMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.isFlying()) return;
+        Block block = player.getLocation().getBlock();
+        if (player.isSneaking() && !block.getType().equals(Material.WATER)) {
+            if (playerLastBlock.get(player) == null || !playerLastBlock.containsKey(player)) {
+                for (Player otherPlayers : Bukkit.getOnlinePlayers()) {
+                    if (otherPlayers.getName().equals(player.getName())) return;
+                    otherPlayers.sendBlockChange(player.getLocation(), Material.BARRIER.createBlockData());
+                    if (otherPlayers.getNearbyEntities(0.5,1.8,0.5).contains(player)) {
+                        otherPlayers.sendBlockChange(player.getLocation(), Material.AIR.createBlockData());
                     }
-
-                } else if (playerLastBlock.containsKey(player)){
-
-                    for (Player otherPlayers : Bukkit.getOnlinePlayers()){
-
-                        if (otherPlayers != null){
-
-                            if (!otherPlayers.getDisplayName().equals(player.getDisplayName())){
-
-                                otherPlayers.sendBlockChange(playerLastBlock.get(player).getLocation(), Material.AIR.createBlockData());
-                                otherPlayers.sendBlockChange(player.getLocation(), Material.BARRIER.createBlockData());
-                                playerLastBlock.put(player, block);
-
-                            }
-
-                        }
-
-                    }
-
+                    playerLastBlock.put(player, block);
                 }
 
-            } else if (!player.isSneaking() && !block.getType().equals(Material.WATER) && block.getType().equals(Material.AIR)){
+            } else if (playerLastBlock.get(player) != null && playerLastBlock.containsKey(player)) {
+                for (Player otherPlayers : Bukkit.getOnlinePlayers()) {
+                    if (otherPlayers.getName().equals(player.getName())) continue;
+                    Block last = playerLastBlock.get(player);
+                    Bukkit.getScheduler().runTask(this, () -> {
+                        if (player.isFlying()) return;
+                        otherPlayers.sendBlockChange(last.getLocation(), last.getType().createBlockData());
+                        otherPlayers.sendBlockChange(player.getLocation(), Material.BARRIER.createBlockData());
+                        playerLastBlock.put(player, block);
+                    });
+                }
+            }
 
-                if (playerLastBlock.containsKey(player)){
-
-                    for (Player otherPlayers : Bukkit.getOnlinePlayers()){
-
-                        if (otherPlayers != null){
-
-                            if (!otherPlayers.getDisplayName().equals(player.getDisplayName())){
-
-                                otherPlayers.sendBlockChange(playerLastBlock.get(player).getLocation(), Material.AIR.createBlockData());
-
-                            }
-
-                            otherPlayers.sendBlockChange(player.getLocation(), Material.AIR.createBlockData());
-                            playerLastBlock.remove(player);
-
-                        }
-                    }
-
-                } else if (!playerLastBlock.containsKey(player)){
-
-                    for (Player otherPlayers : Bukkit.getOnlinePlayers()){
-
-                        if (otherPlayers != null){
-
-                            if (!otherPlayers.getDisplayName().equals(player.getDisplayName())){
-
-                                otherPlayers.sendBlockChange(player.getLocation(), Material.AIR.createBlockData());
-
-                            }
-
-                        }
-
-                    }
-
+        } else if (!player.isSneaking() && !block.getType().equals(Material.WATER)) {
+            if (playerLastBlock.get(player) != null && playerLastBlock.containsKey(player)) {
+                for (Player otherPlayers : Bukkit.getOnlinePlayers()) {
+                    if (playerLastBlock.get(player) == null || otherPlayers.getName().equals(player.getName()))
+                        continue;
+                    Block last = playerLastBlock.get(player);
+                    Bukkit.getScheduler().runTask(this, () -> {
+                        otherPlayers.sendBlockChange(last.getLocation(), block.getType().createBlockData());
+                        otherPlayers.sendBlockChange(player.getLocation(), last.getType().createBlockData());
+                        playerLastBlock.remove(player);
+                    });
                 }
 
+            } else if (playerLastBlock.get(player) == null || !playerLastBlock.containsKey(player)) {
+                for (Player otherPlayers : Bukkit.getOnlinePlayers()) {
+                    if (otherPlayers.getName().equals(player.getName())) continue;
+                    otherPlayers.sendBlockChange(player.getLocation(), block.getType().createBlockData());
+                }
             }
         }
-
     }
-
 }
